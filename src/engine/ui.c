776bd004bf19d7291f92b1042c39130cc58387a5
SG101 <scottgerretsen@icloud.com>
2022-10-24 20:50:01 +0200

    added text and more layout control to ui module

--- src/engine/ui.c
+++ src/engine/ui.c
@@ -10,14 +10,28 @@
 #include <stdint.h>
 
 SDL_Renderer* uiTarget;
-intptr_t uiCurrentSelected;
-intptr_t uiLastSelected;
+typedef struct ui_selection_t {
+	uintptr_t item;
+	short keepSelected;
+	enum {
+		UI_SELECT_DEFAULT,
+		UI_SELECT_TEXT
+	} selectKind;
+	union {
+		struct {
+			long start, end;
+		} text_select;
+	};
+} ui_selection_t;
+
+ui_selection_t uiSelection;
 
 uint32_t uiCurrentMouseState;
 uint32_t uiLastMouseState;
 int uiMouseX, uiMouseY;
 
 SDL_Rect uiCurrentWindow;
+int uiIndentLevel;
 int uiSingleLineHeight = 30;
 int uiLineSpacing = 5;
 int uiWindowPadding = 10;
@@ -25,6 +39,13 @@ int uiTotalHeight;
 int uiRenderSameLine;
 
 SDL_Rect uiNextLineRect;
+
+struct ui_style_t {
+	TTF_Font* font;
+	int indentPixels;
+} uiStyle;
+
+void uiAddPixels(int n);
 
 void uiUpdateMouseState()
 {
@@ -36,21 +57,27 @@ int uiCanSelect(void* ptr)
 {
 	intptr_t item = (intptr_t)ptr;
 	
-	return
-	(uiCurrentSelected == 0 || uiCurrentSelected == item)
-	&& (uiLastSelected == 0 || uiLastSelected == item);
+	return uiSelection.item == item || uiSelection.item == 0;
 }
 
-void uiSelect(void* ptr)
+void uiSelect(void* ptr, short keepActive)
 {
-	uiCurrentSelected = (intptr_t)ptr;
+	uiSelection.item = (intptr_t)ptr;
+	uiSelection.keepSelected = keepActive;
+}
+
+short uiIsSelected(void* ptr)
+{
+	return uiSelection.item == (intptr_t)ptr;
 }
 
 void uiInit(SDL_Renderer* target)
 {
 	uiTarget = target;
-	uiCurrentSelected = uiLastSelected = 0;
+	uiSelection.item = 0;
+	memset(&uiStyle, 0, sizeof(struct ui_style_t));
 	uiUpdateMouseState();
+	uiStyle.indentPixels = 10;
 }
 
 void uiTerminate()
@@ -58,19 +85,61 @@ void uiTerminate()
 	
 }
 
+void uiSetFont(TTF_Font* font)
+{
+	uiStyle.font = font;
+}
+
+// check if a mouse button was pressed on this ui frame
+static inline
+short uiMouseButtonPressed(uint32_t buttonMask)
+{
+	return
+	(buttonMask & uiCurrentMouseState) != 0 && // button is down
+	(buttonMask & uiLastMouseState) == 0; // button was down
+}
+
+// check if a mouse button was released on this ui frame
+static inline
+short uiMouseButtonReleased(uint32_t buttonMask)
+{
+	return
+	(buttonMask & uiLastMouseState) != 0 && // button was down
+	(buttonMask & uiCurrentMouseState) == 0; // button is up
+}
+
+// check if a mouse button is currently pressed
+static inline
+short uiMouseButtonDown(uint32_t buttonMask)
+{
+	return uiCurrentMouseState & buttonMask;
+}
+
+// change the SDL render target
 void uiSetTarget(SDL_Renderer* renderer)
 {
 	uiTarget = renderer;
 }
 
+// begin a new frame
 void uiBeginFrame()
 {
-	uiLastSelected = uiCurrentSelected;
-	uiCurrentSelected = 0;
+	if(uiSelection.keepSelected == 0)
+		uiSelection.item = 0;
 	uiUpdateMouseState();
 }
 
-void uiAddLines(int n)
+// push any number of lines
+void uiSkipLines(int n)
+{
+	while(n-- >= 0)
+	{
+		uiNextLine();
+	}
+}
+
+// push a new line
+void uiNextLine()
 {
 	if(uiRenderSameLine > 0)
 	{
@@ -79,20 +148,22 @@ void uiAddLines(int n)
 	}
 	else
 	{
-		uiAddPixels(n * (uiSingleLineHeight + uiLineSpacing));
+		uiAddPixels(uiSingleLineHeight + uiLineSpacing);
 	}
 }
 
-void uiAddLine() { uiAddLines(1); }
-
+// push the next line down by a number of pixels
 void uiAddPixels(int px)
 {
 	uiTotalHeight += px;
 	uiNextLineRect = uiCurrentWindow;
 	uiNextLineRect.h = uiSingleLineHeight;
 	uiNextLineRect.y += uiTotalHeight;
+	uiNextLineRect.w -= uiIndentLevel * uiStyle.indentPixels * 2;
+	uiNextLineRect.x += uiIndentLevel * uiStyle.indentPixels;
 }
 
+// draw the next n elements on the same line
 void uiSameLine(int n)
 {
 	if(n > 1)
@@ -103,12 +174,12 @@ void uiSameLine(int n)
 	}
 }
 
+// start a window fitting within the given rectangle
 int uiBeginWindow(SDL_Rect* rect, int* isActive)
 {
 	uiTotalHeight = 0;
+	uiIndentLevel = 0;
 	memcpy(&uiCurrentWindow, rect, sizeof(SDL_Rect));
-	uiNextLineRect = uiCurrentWindow;
-	uiNextLineRect.h = uiSingleLineHeight;
 	
 	if(*isActive)
 	{
@@ -116,12 +187,19 @@ int uiBeginWindow(SDL_Rect* rect, int* isActive)
 		SDL_RenderFillRect(uiTarget, rect);
 	}
 	
+	uiNextLineRect = uiCurrentWindow;
+	uiNextLineRect.h = uiSingleLineHeight;
 	int toggled = uiButton();
 	
 	uiCurrentWindow.x += uiWindowPadding;
 	uiCurrentWindow.y += uiWindowPadding;
 	uiCurrentWindow.w -= uiWindowPadding*2;
 	uiCurrentWindow.h -= uiWindowPadding*2;
+	
+	uiNextLineRect.x = uiCurrentWindow.x;
+	uiNextLineRect.w = uiCurrentWindow.w;
+	uiNextLineRect.y = uiCurrentWindow.y + uiTotalHeight;
+	uiNextLineRect.h = uiSingleLineHeight;
 	
 	if(toggled)
 	{
@@ -133,6 +211,7 @@ int uiBeginWindow(SDL_Rect* rect, int* isActive)
 	return (*isActive);
 }
 
+// check if point (x,y) is in r
 int uiInArea(SDL_Rect* r, int x, int y)
 {
 	int x_min = r->x, x_max = r->x + r->w;
@@ -140,6 +219,34 @@ int uiInArea(SDL_Rect* r, int x, int y)
 	return (x >= x_min && x < x_max && y >= y_min && y < y_max);
 }
 
+void uiDrawText(const char* text, SDL_Rect dstrect, SDL_Color colour)
+{
+	int height = TTF_FontHeight(uiStyle.font);
+	double fontToLineHeight = (double)height/(double)uiSingleLineHeight;
+	
+	SDL_Rect srcrect = { 0, 0, dstrect.w * fontToLineHeight, dstrect.h * fontToLineHeight };
+	SDL_Surface* surface = TTF_RenderText_Solid_Wrapped(uiStyle.font, text, colour, srcrect.w);
+	
+	dstrect.w = surface->w / fontToLineHeight;
+	
+	if(surface == NULL)
+	{
+		SDL_Log("Failed to render text to surface:\n%s", SDL_GetError());
+		return;
+	}
+	
+	SDL_Texture* tex = SDL_CreateTextureFromSurface(uiTarget, surface);
+	SDL_FreeSurface(surface);
+	
+	if(tex == NULL)
+		return;
+	
+	SDL_RenderCopy(uiTarget, tex, &srcrect, &dstrect);
+	
+	SDL_DestroyTexture(tex);
+}
+
+// draw an interactive slider with a min, max and step
 int uiSlider(float* value, float min, float max, float step)
 {
 	const int SLIDER_WIDTH = 10;
@@ -160,19 +267,19 @@ int uiSlider(float* value, float min, float max, float step)
 	
 	SDL_Color sliderColor = {230, 230, 230, 255};
 
-	if(uiCurrentMouseState & SDL_BUTTON_LEFT)
+	if(uiMouseButtonPressed(SDL_BUTTON_LEFT))
 	{
 		if(uiCanSelect(value) && uiInArea(&position, uiMouseX, uiMouseY))
 		{
-			uiSelect(value);
+			uiSelect(value, 1);
 		}
 	}
-	else if(uiCurrentSelected == (intptr_t)value)
+	else if(uiIsSelected(value) && !uiMouseButtonDown(SDL_BUTTON_LEFT))
 	{
-		uiSelect(NULL);
+		uiSelection.keepSelected = 0;
 	}
 	
-	if(uiCurrentSelected == (intptr_t)value)
+	if(uiIsSelected(value))
 	{
 		valuePercentage = (float)(uiMouseX - position.x) / (float)(position.w);
 		
@@ -182,7 +289,9 @@ int uiSlider(float* value, float min, float max, float step)
 			valuePercentage = 1;
 		
 		(*value) = valuePercentage * (max - min);
-		(*value) = floorf((*value) * (1.f/step)) * step + min;
+		(*value) = roundf((*value) * (1.f/step)) * step + min;
+		valuePercentage = ((*value) - min) / (max-min);
+		
 		
 		if(*value != startValue)
 		{
@@ -197,28 +306,82 @@ int uiSlider(float* value, float min, float max, float step)
 	
 	SDL_SetRenderDrawColor(uiTarget, 100, 100, 100, 255);
 	SDL_RenderFillRect(uiTarget, &lineRect);
-	SDL_SetRenderDrawColor(uiTarget, sliderColor.r, sliderColor.g, sliderColor.b, sliderColor.a);
-	SDL_RenderFillRect(uiTarget, &sliderRect);
 	
-	uiAddLine();
+	if(valuePercentage >= 0 && valuePercentage <= 1)
+	{
+		SDL_SetRenderDrawColor(uiTarget, sliderColor.r, sliderColor.g, sliderColor.b, sliderColor.a);
+		SDL_RenderFillRect(uiTarget, &sliderRect);
+	}
+	
+	uiNextLine();
+	
 	return valueChanged;
 }
 
+// draw a clickable button
 int uiButton()
 {
 	SDL_Rect position = uiNextLineRect;
+	short clicked = 0;
+	
+	if(uiMouseButtonPressed(SDL_BUTTON_LEFT))
+	{
+		if(uiCanSelect(0) && uiInArea(&position, uiMouseX, uiMouseY))
+		{
+			uiSelect(0, 0);
+			clicked = 1;
+		}
+	}
 	
 	SDL_SetRenderDrawColor(uiTarget, 100, 100, 100, 255);
 	SDL_RenderFillRect(uiTarget, &position);
 	
-	uiAddLine();
-	
-	if((uiLastMouseState & SDL_BUTTON_LEFT) == 0 && (uiCurrentMouseState & SDL_BUTTON_LEFT) != 0)
-	{
-		if(uiCanSelect(0) && uiInArea(&position, uiMouseX, uiMouseY))
-		{
-			return 1;
-		}
-	}
-	return 0;
+	uiNextLine();
+
+	return clicked;
+}
+
+void uiLabel(const char* label)
+{
+	SDL_Colour white = {255, 255, 255, 255};
+	uiDrawText(label, uiNextLineRect, white);
+	uiNextLine();
+}
+
+void uiLabelNext(const char* label, float ratio)
+{
+	int y = uiNextLineRect.y;
+	uiNextLineRect.w = uiCurrentWindow.w * ratio;
+	uiLabel(label);
+	uiTotalHeight -= uiSingleLineHeight + uiLineSpacing;
+	uiNextLineRect.y = y;
+	uiNextLineRect.x += uiCurrentWindow.w * ratio;
+	uiNextLineRect.w = uiCurrentWindow.w * (1-ratio);
+	uiNextLineRect.h = uiSingleLineHeight;
+}
+
+void uiHeader(const char* label)
+{
+	uiSubIndent();
+	uiAddPixels(uiSingleLineHeight/2);
+	uiLabel(label);
+	uiAddIndent();
+}
+
+void uiSetIndent(int n)
+{
+	uiIndentLevel = n;
+	uiIndentLevel = uiIndentLevel >= 0 ? uiIndentLevel : 0;
+	// force update of next line rect
+	uiAddPixels(0);
+}
+
+void uiAddIndent()
+{
+	uiSetIndent(uiIndentLevel + 1);
+}
+
+void uiSubIndent()
+{
+	uiSetIndent(uiIndentLevel - 1);
 }
--- src/engine/ui.h
+++ src/engine/ui.h
@@ -9,24 +9,30 @@
 #define ui_h
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 
 extern void uiInit(SDL_Renderer* target);
 extern void uiTerminate();
 
 extern void uiSetTarget(SDL_Renderer* renderer);
 
+extern void uiSetFont(TTF_Font* font);
+
 extern void uiBeginFrame();
 
 extern int uiBeginWindow(SDL_Rect* rect, int* isActive);
 
-extern void uiEndWindow();
-
-extern void uiAddLines(int n);
-extern void uiAddLine();
-extern void uiAddPixels(int px);
+extern void uiSkipLines(int n);
+extern void uiNextLine();
 extern void uiSameLine(int n);
 
 extern int uiSlider(float* value, float min, float max, float step);
 extern int uiButton();
+extern void uiLabel(const char* label);
+extern void uiLabelNext(const char* label, float ratio);
+extern void uiHeader(const char* label);
+extern void uiAddIndent();
+extern void uiSubIndent();
+extern void uiSetIndent(int n);
 
 #endif /* ui_h */
